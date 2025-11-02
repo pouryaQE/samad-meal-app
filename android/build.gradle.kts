3e99b5773adb7504f657c5fc465f5295f7930d7f
pouryaQE <pouryaqe@example.com>
2025-11-03 03:20:47 +0330

    Initial commit - Samad Meal App

---
+++ android/build.gradle.kts
@@ -0,0 +1,26 @@
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.iran.liara.run") }
+        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
